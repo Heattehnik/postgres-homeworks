e92bfc7ec07ad0fd80012150f4ff6681f4f2af77
Heattehnik <gadjka@mail.ru>
2023-05-16 20:53:07 +0500

    modify create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,22 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+    first_name varchar(50) not null,
+    last_name  varchar(50) not null,
+    title      varchar(50) not null,
+    birthday   date        not null,
+    notes      text
+);
+create table customers (
+    customer_id varchar(10) UNIQUE not null,
+    company_name varchar(100) not null,
+    contact_name varchar(100) not null
+);
+create table orders (
+    order_id serial not null,
+    customer_id varchar(10) references customers(customer_id),
+    employee_id int not null,
+    order_date date not null,
+    ship_city varchar(50)
+
+);
